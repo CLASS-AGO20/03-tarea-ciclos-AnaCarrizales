2f664958005a88d8968c365536953889317ba57e
Ana Carrizales <acarrizales2@ucol.mx>
2020-12-07 23:32:55 -0600

    sumatoriaSerieUno was created and tested

--- src/main.js
+++ src/main.js
@@ -1,3 +1,19 @@
-export default class App {}
+export default class App {
+
+    sumatoriaSerieUno(numero)
+    {
+        let operacion = 0;
+        for(let i = 1; i <= numero; i++)
+        {
+            operacion = 1/numero;
+        }
+        return operacion;
+    }
+
+
+
+
+}
 
 let app = new App();
+console.log(app.sumatoriaSerieUno(5));
